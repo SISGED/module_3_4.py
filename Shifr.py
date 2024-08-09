dc3472bf55697f8845237e25da8d265f7e2beb77
SISGED <spicin1995@mail.ru>
2024-08-09 10:32:12 +0300

    next day

--- Shifr.py
+++ Shifr.py
@@ -1,9 +1,7 @@
-
-for a in range (3, 21):
+for a in range(3, 21):
     result = f'{a} - '
-    for i in range (1, a):
-        for j in range (i+1, a):
+    for i in range(1, a):
+        for j in range(i + 1, a):
             if a % (i + j) == 0:
                 result += str(i) + str(j)
     print(result)
-
